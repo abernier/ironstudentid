5b1d8243078fcd0cc5c2e3619e58342003214e9d
Antoine BERNIER <antoine.bernier@gmail.com>
2021-02-24 09:17:43 +0100

    wdpt202102par

--- ironstudentid.user.js
+++ ironstudentid.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ironstudentid
-// @version      1.0.2
+// @version      1.0.3
 // @author       abernier
 // @namespace    name.abernier
 // @description  change some IDs into students' names on https://metabase.ironhack.tech/*
@@ -21,6 +21,23 @@ console.log('ironstudents', document.body)
 //
 
 const students = {
+    // wdpt202102par
+    '5f7727256fab06000aa7c360': 'Ting-Chi',
+    '5ff47e3ffd5493002d13ad7d': 'Tran',
+    '5ffddd79a37993002d491592': 'Sofia',
+    '5fa911d2720769002d02b967': 'Walid',
+    '5ffc160037ecbf002d4c05c7': 'Michel',
+    '5f620e05180b66000cb8af99': 'Mathilde',
+    '5f3a3876cf7913000c3826bf': 'Massi',
+    '5fdc81c28cafa8002d9570a7': 'Laurent',
+    '5fea109dc7f3e5002d44f877': 'Laura',
+    '5fc4c8e0b0342d002dd486d8': 'Karina',
+    '5ffd89ffa37993002d491587': 'Julien',
+    '5fd0e86e5d385c002dcd55b1': 'Chloé',
+    '600aa13ca91c51002d7a747f': 'Alexandre Christacos',
+    '5fdc8d7e8cafa8002d9570aa': 'Alexandre Capaldi',
+    '5fbce39208fc1a002d5e5f11': 'Adam',
+    // wdpt202006par
     '5ea947bae2a33d000c7f13c7': 'Wafaa',
     '5e5524ebb3dcf00016a9c1d5': 'Thomas',
     '5ebe873d269a52000cdbcd44': 'Thi Trang',
