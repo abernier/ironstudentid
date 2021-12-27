de5ce143029b2c29f031d54a91b881b4a7a86830
Antoine BERNIER <antoine.bernier@gmail.com>
2021-12-27 13:59:35 +0100

    Update ironstudentid.user.js

--- ironstudentid.user.js
+++ ironstudentid.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         ironstudentid
-// @version      1.1.1
+// @version      1.1.2
 // @author       abernier
 // @namespace    name.abernier
 // @description  change some IDs into students' names on https://metabase.ironhack.tech/*
@@ -71,7 +71,7 @@ const students = {
 // Observe DOM changes
 const observer = new MutationObserver((mutationsList, observer) => {
     // Replace IDs by students' names
-    document.querySelectorAll('.cellData, .tether-target').forEach($cell => {
+    document.querySelectorAll('.cellData, [class*="LegendItemstyled__LegendItemTitle"] >div').forEach($cell => {
         //console.log('$cell', $cell)
         const studentId = $cell.textContent.trim()
 
